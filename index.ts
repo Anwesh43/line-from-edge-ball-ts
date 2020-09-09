7b81e53248fdf31625befe1715fc40e5ffe60d6b
Anwesh43 <anweshthecool0@gmail.com>
2020-09-09 20:16:31 +0530

    adding LineFromEdgeCircle class

--- index.ts
+++ index.ts
@@ -192,3 +192,26 @@ class LFECNode {
 
     }
 }
+
+class LineFromEdgeCircle {
+
+    curr : LFECNode = new LFECNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
