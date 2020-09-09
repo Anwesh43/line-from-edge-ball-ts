b73827c60a117f0b4e44481aed8f4a92d64e2528
Anwesh43 <anweshthecool0@gmail.com>
2020-09-09 19:16:25 +0530

    adding DrawingUtil class

--- index.ts
+++ index.ts
@@ -14,6 +14,7 @@ const colors : Array<string> = [
     "#FF5722"
 ] 
 const backColor : string = "#FF5722"
+const rFactor : number = 9.6 
 
 class ScaleUtil {
 
@@ -27,5 +28,49 @@ class ScaleUtil {
 
     static sinify(scale : number) : number {
         return Math.sin(scale * Math.PI)
+    }
+}
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawCircle(context : CanvasRenderingContext2D, x : number, y : number, r : number) {
+        context.beginPath()
+        context.arc(x, y, r, 0, 2 * Math.PI)
+        context.fill()
+    }
+
+    static drawLineFromEdge(context : CanvasRenderingContext2D, scale : number) {
+        const r : number = Math.min(w, h) / sizeFactor 
+        const sf : number = ScaleUtil.sinify(scale)
+        const sf1 : number = ScaleUtil.divideScale(sf, 0, parts)
+        const sf2 : number = ScaleUtil.divideScale(sf, 1, parts)
+        const sf3 : number = ScaleUtil.divideScale(sf, 2, parts)
+        const gap : number = h * 0.5 - w * 0.5
+        context.save()
+        context.translate(w / 2, h / 2)
+        context.rotate(rot * sf3)
+        for (var j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1)
+            DrawingUtil.drawLine(context, 0, 0, -w / 2 + w * 0.5 * sf1 + gap * sf3, 0)
+            context.restore()
+        }
+        DrawingUtil.drawCircle(context, 0, 0, r * sf2)
+        context.restore()
+    }
+
+    static drawLFECNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawLineFromEdge(context, scale)
     }
 }
